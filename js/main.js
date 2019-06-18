97ad662127525137e686de1efae43f32ae3baf48
vicky1903 <50952787+vicky1903@users.noreply.github.com>
2019-06-18 16:41:31 +0200

    finish !

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
     //on met tous le code jquery a l'interieur//
     //en un click on colore ou non chaque rond//
-   $('.row div').click(function(){
+   $('.rond').click(function(){
        //façon compliquer de faire (comme lucy)
         /* if ($(this).hasClass('active')){
         $(this).removeClass('active');}
@@ -10,18 +10,44 @@ $(document).ready(function(){
         $(this).toggleClass('active')
     });
 
-    //button rempli
+    //buton rempli
     $('#remplir').click(function(){
         $('.row div').addClass('active');
     });
-    //vider
+    //bouton vider
     $('#vider').click(function(){
         $('.row div').removeClass('active')
     });
 
-    $('.carre').click(function(){
-        $('')
-    })
+    //si click carre 1
+    $('.c1 ').click(function(){
+        if ($('.row1').hasClass('active')){
+            $('.row1').removeClass('active');}
+            else {$('.row1').addClass('active');}
+    });
+    //carré 2
+    $('.c2').click(function(){
+        if ($('.row2').hasClass('active')){
+            $('.row2').removeClass('active');}
+            else {$('.row2').addClass('active');}
+    });
+    //carré4
+    $('.c4').click(function(){
+        if ($('.row4').hasClass('active')){
+            $('.row4').removeClass('active');}
+            else {$('.row4').addClass('active');}
+    });
+    //carré5
+    $('.c5').click(function(){
+        if ($('.row5').hasClass('active')){
+            $('.row5').removeClass('active');}
+            else {$('.row5').addClass('active');}
+    });
 
-    
+    //si clique losange colonne et rang s activent
+    $('.los1 ').click(function(){
+        if ($('.row3,.col3').hasClass('active')){
+            $('.row3,.col3').removeClass('active');}
+            else {$('.row3,.col3').addClass('active');}
+    });
 })

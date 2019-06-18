9f976914d9179ae119eb18af581d5a43b52de7ba
vicky1903 <50952787+vicky1903@users.noreply.github.com>
2019-06-18 15:27:14 +0200

    bouton ok

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 //chargement du code et executer des que possible
 $(document).ready(function(){
-    //on met tous le code jquery a l'interieur
-    //en un click ol colore ou non chaque rond
+    //on met tous le code jquery a l'interieur//
+    //en un click on colore ou non chaque rond//
     $('.rond').click(function(){
         if ($(this).hasClass('active')){
         $(this).removeClass('active');}
         else {$(this).addClass('active');} 
     });
+
+    //button rempli
+    $('#remplir').click(function(){
+        $('.row div').addClass('active');
+    });
+    //vider
+    $('#vider').click(function(){
+        $('.row div').removeClass('active')
+    });
+
+
     
-    
-});
+})
